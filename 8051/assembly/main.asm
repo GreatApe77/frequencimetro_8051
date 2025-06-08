c6dccadac7c346bee526a5731b14181cf024a946
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-06-08 18:56:57 -0300

    change logic to cache bin_to_bcd result

--- 8051/assembly/main.asm
+++ 8051/assembly/main.asm
@@ -2,18 +2,31 @@
 ;Microcontralador:                      AT89C52-24PC
 ;Frequência do cristal oscilador:       22,1184 MHz
 ;Projeto:                               Frequêncimetro digital ~65 KHz
+;Alunos: Mateus e Rebeca
 ;====================================================================
+
+;====================================================================
+; CONSTANTES
+;====================================================================
+
 TIMER0_COUNTER		equ		65535-46080+8
-TIMES_INTERRUPT         equ		40d             ;20d
+TIMES_INTERRUPT         equ		40d
 ;====================================================================
 ; VARIAVEIS
 ;====================================================================
+
 milisegundos            equ             0Ah
 fHighByte               equ             0Bh
 fLowByte                equ             0Ch
 binToBcdHigh            equ             0Dh
 binToBcdLow             equ             0Eh
 flagPassou1Seg          bit             20h.0
+
+displayUni		equ		30h
+displayDez		equ		31h
+displayCent		equ		32h
+displayUniMilhar	equ		33h
+displayDezMilhar	equ		34h
 
 ;====================================================================
 ; PROGRAMA
@@ -27,15 +40,12 @@ setup:
         call            config_timers
 restart:
 
-;=========
         mov             TMOD,#21h       ;Configura Timer 1 no modo 2 (8 bits com recarga automática)
         mov             SCON,#50h       ;Configura serial no modo 1 (Habilitando recepcao)
         mov             TH1,#0FAh       ;Carga de TH1 para um clock de 22.1184Mhz
         mov             TL1,TH1
         setb            TR1
-;============
-        ;clr            TR1
-        ;call           delay
+
 loop_displays:
         call            atualizar_displays      
         jnb             RI,loop_displays
@@ -53,7 +63,15 @@ loop_displays:
         clr             flagPassou1Seg
         mov             fLowByte,TL1
         mov             fHighByte,TH1
-        ;;;TRANSMITIR SERIAL
+	mov            	binToBcdHigh,fHighByte
+	mov             binToBcdLow,fLowByte
+	call		bin24_to_bcd
+	mov		displayUni,R1
+	mov		displayDez,R2
+	mov		displayCent,R3
+	mov		displayUniMilhar,R4
+	mov		displayDezMilhar,R5
+	; INICIO DA TRANSMISSAO SERIAL
         call            config_serial_transmissao
         setb            TR1
         mov             A,fHighByte
@@ -64,7 +82,7 @@ loop_displays:
         mov             SBUF,A
         jnb             TI,$
         clr             TI
-        call            reset_contexto_serial
+	; FIM DA TRANSMISSAO SERIAL
         ajmp            restart
 
 
@@ -85,7 +103,6 @@ config_timers:
         mov             TMOD,#01010001b ;Configura Timer 0 e 1
         mov             TL1,#00h        ;Zera Timer1 Low
         mov             TH1,#00h        ;Zera Timer1 High (o Valor da frequência será medido desse timer como contador)
-        ;mov            milisegundos,#100d              ;Carrega a variavel milisegundos com o valor 20.Representa o número de estouros para contar 1 segundo
         mov             milisegundos,#TIMES_INTERRUPT
         clr             flagPassou1Seg                ;Limpa a flag que indica o passar de 1 segundo
         ret
@@ -94,27 +111,15 @@ config_timers:
 ;Configura Serial para transmissão da frequência
 ;
 config_serial_transmissao:
-        ;push           TMOD
-        ;push           SCON
-        
-        ;orl            IE,#90h         ;habilita interrupcao Serial
         mov             TMOD,#21h       ;Configura Timer 1 no modo 2 (8 bits com recarga automática)
         mov             SCON,#40h       ;Configura serial no modo 1 
         mov             TH1,#0FAh       ;Carga de TH1 para um clock de 22.1184Mhz
         mov             TL1,TH1
         ret
 
-;
-;Configura Serial para transmissão da frequência
-;
-reset_contexto_serial:
-        pop             SCON
-        pop             TMOD
-        ret
+
 atualizar_displays:
-        mov             binToBcdHigh,fHighByte
-        mov             binToBcdLow,fLowByte
-        call            bin24_to_bcd
+	
         call            atualizar_display_frequencia
         call            atualizar_display_unidade
         call            atualizar_display_dezena
@@ -123,6 +128,9 @@ atualizar_displays:
         call            atualizar_display_dezena_milhar
         ret
 
+;
+;Escreve a letra F. na P0
+;
 atualizar_display_frequencia:
         mov             A,#00001110b    ;Move os bits necessário para formar a letra 'F'
         mov             P0,A
@@ -130,46 +138,55 @@ atualizar_display_frequencia:
         call            delay
         setb            P2.0
         ret
-        
-;PARAMETROS: R1 com o valor das unidades
+;        
+;Atualiza o display das unidades
+;
 atualizar_display_unidade:
-        mov             A,R1
+        mov             A,displayUni
         call            convert
         mov             P0,A
         clr             P2.5
         call            delay
         setb            P2.5
         ret
-;PARAMETROS: R2 com o valor das dezenas
+;
+;Atualiza o display das dezenas
+;
 atualizar_display_dezena:
-        mov             A,R2
+        mov             A,displayDez
         call            convert
         mov             P0,A
         clr             P2.4
         call            delay
         setb            P2.4
         ret
-;PARAMETROS: R3 com o valor das centenas
+;
+;Atualiza o display das centenas
+;
 atualizar_display_centena:
-        mov             A,R3
+        mov             A,displayCent
         call            convert
         mov             P0,A
         clr             P2.3
         call            delay
         setb            P2.3
         ret
-;PARAMETROS: R4 com o valor das unidades de milhar
+;
+;Atualiza o display das unidades de milhar
+;
 atualizar_display_unidade_milhar:
-        mov             A,R4
+        mov             A,displayUniMilhar
         call            convert
         mov             P0,A
         clr             P2.2
         call            delay
         setb            P2.2
         ret
-;PARAMETROS: R5 com o valor das dezenas de milhar
+;
+;Atualiza o display das dezenas de milhar
+;
 atualizar_display_dezena_milhar:
-        mov             A,R5
+        mov             A,displayDezMilhar
         call            convert
         mov             P0,A
         clr             P2.1
@@ -177,7 +194,9 @@ atualizar_display_dezena_milhar:
         setb            P2.1
         ret
 
-
+;
+;Rotina responsável por tratar cada estouro do timer 0 como temporizador.
+;
 tratar_interrupcao:
         mov             TL0, #low TIMER0_COUNTER
         mov             TH0, #high TIMER0_COUNTER
@@ -188,12 +207,20 @@ tratar_interrupcao:
         
 saida:
         reti
+
+;
+;Rotina para converter um nibble para sua representação no display de 7 segmentos.
+;Recebe o Acumulador como parametro e altera seu valor
+;
 convert:
         anl             A,#0Fh
         mov             DPTR ,#table
         movc            A,@A+DPTR
         cpl             A
         ret
+;
+;Tabela de mapeamento numero => representação no display catodo comum (precisa ser invertida no displaay anodo comum)
+;
 table:
         DB              00111111B       ;0
         DB              00000110B       ;1
@@ -211,6 +238,15 @@ table:
         DB              01011110B       ;D
         DB              01111001B       ;E
         DB              01110001B       ;F
+
+;
+;Rotina que recebe 2 bytes(binToBcdHigh,binToBcdLow)
+;e escreve sua representação decimal (0 até 65535) nos registradores de 1 a 5
+;R1 <- unidade
+;R2 <- dezena
+;R3 <- centena
+;R4 <- unidade de milhar
+;R5 <- dezena de milhar
 
 bin24_to_bcd:
         mov             R1,#00h
@@ -264,4 +300,6 @@ exit:
         ret
         
 ;====================================================================
-        END
+; FIM DO PROGRAMA
+;====================================================================
+	end
